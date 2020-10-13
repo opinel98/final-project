dd75bedba72b6c7497aa475750c914b547ace96e
Isabel Morales <imoralessirgo@outlook.com>
2020-10-13 19:19:39 -0400

    add auth

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 const express = require("express");
 const app = express();
 
+let online = null;
 let userdb = null;
 
 
@@ -27,10 +28,31 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/game.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+app.get("/", (request, response) => {
+  response.redirect("/login")
+});
+
+app.get("/login", (request,response)=>{
+  response.render("login.html");
+})
+
+app.post('/login', function(req, res){
+  if(!req.body.id || !req.body.password){
+    res.status("400");
+    res.send("Invalid details!");
+  } else {
+    console.log("made it")
+
+    var newUser = {id: req.body.id, password: req.body.password};
+    userdb.findOne({id : req.body.id, password: req.body.password}, function (err, result) {
+      if(!result){ // if user does not exist, register a new user
+        userdb.insertOne(newUser)
+      }
+    })
+    req.session.user = newUser;
+    online = req.session.user;
+    res.redirect('/home');
+  }
 });
 
 // listen for requests :)
