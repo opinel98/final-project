d01e10d8bb35a5541bc35260a3f249a2acdd8d1d
jcybul <jcybul@wpi.edu>
2020-10-14 20:49:44 -0400

    attempting to have sessions

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ var bodyParser = require('body-parser')
 var session = require('express-session');
 var cookieParser = require('cookie-parser')
 
-
-
 app.use(cookieParser());
 app.use(session({secret: "Your secret key"}));
 
@@ -24,19 +22,15 @@ app.use(session({secret: "Your secret key"}));
 const saltRounds = 10;
 const bcrypt = require('bcrypt');
 
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.set('views',__dirname + '/views');
 
 let online = null;
 let userdb = null;
 
-online = "Joe";
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://dbUser:dbPassword@cluster0.ui701.mongodb.net/<dbname>?retryWrites=true&w=majority";
@@ -47,39 +41,49 @@ client.connect(err => {
 });
 
 
-
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/player_lobby.html");
-});
-
-
 
 // set up socket io for the chat room
 let io = socket.listen(server);
 io.sockets.on('connection', function () {
   console.log('hello world im a hot socket');
 });
-
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-
-io.on('connection', (socket) => {
-  socket.broadcast.emit('hi');
-});
+ // This will emit the event to all connected sockets
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('chat message', (msg,req) => {
+    io.emit('chat message',    + ": " +msg);
   });
 });
 
 app.get("/", (request, response) => {
   console.log("in login")
   response.redirect("/login")
+});
+
+app.get("/logged_chat", (request, response) => {
+  console.log("in chat")
+  response.sendFile(__dirname + "/views/player_lobby_chat.html");
+});
+
+app.post("/onload", (request, response) => {
+  let s  = {uname:request.session.user}
+  console.log(s);
+  response.json(s);
+})
+
+app.get("/logged", (request, response) => {
+  if(request.session.id) {
+    console.log(request.session.user);
+    response.render("player_lobby.html")
+  }
+  else{
+    response.redirect('/login');
+  }
 });
 
 app.post("/login" ,async(request,response) => {
@@ -97,8 +101,11 @@ app.post("/login" ,async(request,response) => {
             bcrypt.compare(pin,res[0].password, function(err, c) {
               if (c === true) {
                 console.log("Autenticated")
+                request.session.user = uin;
+                console.log(uin);
+                console.log(request.session.user);
                 online = uin;
-                response.redirect("/");
+                response.redirect("/logged")
               }
               else {
                 let m = "Fail"
@@ -115,7 +122,7 @@ app.post("/login" ,async(request,response) => {
           console.log(hash);
           newUser = {id: uin,password: hash}
           userdb.insertOne(newUser)
-          online = uin;
+          request.session.id = uin;
           console.log("new user created")
           response.redirect("/");
         });
@@ -130,12 +137,10 @@ app.get("/login", (request,response)=>{
   response.render("login.html");
 })
 
-
-
-
-
-
 // listen for requests :)
 server.listen(3000,() => {
   console.log('listening on *:3000');
 });
+
+
+
